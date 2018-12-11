87bcbe53b49e6b1c22e9c108ae76027bd2ce7b3f
michaelnunzio <michaelnunzio12@gmail.com>
2018-12-10 21:30:05 -0500

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports= function(app){
 app.get("/", function(req, res) {
   console.log(path.join(__dirname, "../public/home.html"))
   console.log(path.join(__dirname))
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "../../public/home.html"));
   });
 
   app.get("/survey", function(req, res) {
